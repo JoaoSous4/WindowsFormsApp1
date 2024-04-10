1ba2bec10d882821efe04fc929a6902be24a2b20
Joao <joaovictor201056@hotmail.com>
2024-04-10 14:39:09 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/frm_login.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txb_user = new System.Windows.Forms.TextBox();
+            this.txb_passw = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Logar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // txb_user
+            // 
+            this.txb_user.Location = new System.Drawing.Point(85, 83);
+            this.txb_user.Name = "txb_user";
+            this.txb_user.Size = new System.Drawing.Size(159, 20);
+            this.txb_user.TabIndex = 2;
+            this.txb_user.Click += new System.EventHandler(this.txb_user_Click);
+            this.txb_user.TextChanged += new System.EventHandler(this.txb_user_TextChanged);
+            // 
+            // txb_passw
+            // 
+            this.txb_passw.Location = new System.Drawing.Point(85, 136);
+            this.txb_passw.Name = "txb_passw";
+            this.txb_passw.Size = new System.Drawing.Size(159, 20);
+            this.txb_passw.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 277);
+            this.Controls.Add(this.txb_passw);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txb_user);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frm_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txb_user;
+        private System.Windows.Forms.TextBox txb_passw;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/frm_login.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class frm_login : Form
+    {
+        MySqlConnection con = new MySqlConnection("SERVER=localhost ; DATABASE=teste ; UID=life ; PASSWORD=123 ;");
+
+        public frm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM user WHERE username = '" + txb_user.Text + "' AND password = '"+txb_passw.Text+"'";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+
+            adapter.Fill(dt);
+
+            if (dt.Rows.Count == 1)
+            {
+                frm_principal principal = new frm_principal();
+                this.Hide();
+                principal.Show();
+
+            }
+            else { 
+                MessageBox.Show("Credenciais incorretas");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txb_user_TextChanged(object sender, EventArgs e)
+        {
+            e.ToString().Trim();
+
+        }
+
+        private void txb_user_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
